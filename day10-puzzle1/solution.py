de7eb1ab1223014d4df9343f7f052c6e7ce6b5bb
Ben Howlett <ben@benhowlett.com>
2023-12-18 13:00:05 -0700

    Day 10 sync

--- day10-puzzle1/solution.py
+++ day10-puzzle1/solution.py
@@ -21,13 +21,13 @@ class Path:
         return self.nodes[-2].loc
     
     def get_next_node_loc(self):
-        print("Next node location info\n", self.nodes[-1].connections)
+        # print("Next node location info\n", self.nodes[-1].connections)
         for connection in self.nodes[-1].connections:
-            for node in self.nodes:
-                print(node.loc)
-            print("current node loc:", self.nodes[-1].loc)
-            print("connection in current node:", connection)
-            print("previous node loc:", self.nodes[-2].loc)
+            # for node in self.nodes:
+            #     print(node.loc)
+            # print("current node loc:", self.nodes[-1].loc)
+            # print("connection in current node:", connection)
+            # print("previous node loc:", self.nodes[-2].loc)
             if connection != self.nodes[-2].loc: 
                 return connection
         return ()
@@ -73,15 +73,18 @@ map[startLoc[0]][startLoc[1]].connections = startConnections
 
 paths = [Path(map[startLoc[0]][startLoc[1]]), Path(map[startLoc[0]][startLoc[1]].connections)]
 
-for i, path in enumerate(paths):
-    for connection in map[startLoc[0]][startLoc[1]].connections:
-        paths[i].add_node_to_path(map[connection[0]][connection[1]])
-        # print(path.get_path_len())
+maxPathFound = False
 
-foundMaxPath = False
+while not maxPathFound:
+    for i, path in enumerate(paths):
+        if path.get_path_len() == 0:
+            currentNode = path.nodes[-1]
+            print(currentNode)
+            nextLoc = currentNode.connections[i]
+            path.add_node_to_path(map[nextLoc[0]][nextLoc[1]])
+    maxPathFound = True
 
-print(paths[0].get_current_node().loc, paths[0].get_next_node_loc(), startLoc)
+print(paths[0].get_current_node().loc, paths[0].get_next_node_loc())
+print(paths[0].get_current_node().loc, paths[0].get_next_node_loc())
 
-while not paths[0].get_next_node_loc() == startLoc:
-    print(paths[0].get_current_node().loc)
-    paths[0].add_node_to_path(map[paths[0].get_next_node_loc()[0]][paths[0].get_next_node_loc()[0]])
+
